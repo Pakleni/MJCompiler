c5f183310d9460b200979e3c873065c3a524b3ed
Ognjen Bjeletic <ognjenbjel@protonmail.com>
2022-09-10 21:21:52 +0200

    cleanup

--- src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
+++ src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
@@ -13,30 +13,7 @@ import java.util.Stack;
 
 public class SemanticAnalyzer extends VisitorAdaptor {
 
-	Obj currentMethod = null;
-	boolean returnFound = false;
-	boolean errorDetected = false;
-	int nVars;
-
-	Logger log = Logger.getLogger(getClass());
-
-	public void report_error(String message, SyntaxNode info) {
-		errorDetected = true;
-		StringBuilder msg = new StringBuilder(message);
-		int line = (info == null) ? 0 : info.getLine();
-		if (line != 0)
-			msg.append(" na liniji ").append(line);
-		log.error(msg.toString());
-	}
-
-	public void report_info(String message, SyntaxNode info) {
-		StringBuilder msg = new StringBuilder(message);
-		int line = (info == null) ? 0 : info.getLine();
-		if (line != 0)
-			msg.append(" na liniji ").append(line);
-		log.info(msg.toString());
-	}
-
+	// region "Program"
 	public void visit(Program program) {
 		nVars = MyTab.currentScope.getnVars();
 		MyTab.chainLocalSymbols(program.getProgName().obj);
@@ -47,6 +24,7 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 		progName.obj = MyTab.insert(Obj.Prog, progName.getValue(), MyTab.noType);
 		MyTab.openScope();
 	}
+	// endregion
 
 	// region "Type"
 	Struct temporaryType = null;
@@ -512,6 +490,30 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 	// TODO: conditions
 
 	// region "Helpers"
+	Obj currentMethod = null;
+	boolean returnFound = false;
+	boolean errorDetected = false;
+	int nVars;
+
+	Logger log = Logger.getLogger(getClass());
+
+	public void report_error(String message, SyntaxNode info) {
+		errorDetected = true;
+		StringBuilder msg = new StringBuilder(message);
+		int line = (info == null) ? 0 : info.getLine();
+		if (line != 0)
+			msg.append(" na liniji ").append(line);
+		log.error(msg.toString());
+	}
+
+	public void report_info(String message, SyntaxNode info) {
+		StringBuilder msg = new StringBuilder(message);
+		int line = (info == null) ? 0 : info.getLine();
+		if (line != 0)
+			msg.append(" na liniji ").append(line);
+		log.info(msg.toString());
+	}
+
 	public void checkParams(String label, SyntaxNode syntaxNode) {
 		Obj obj = MyTab.find(label);
 
@@ -585,10 +587,9 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 			}
 		}
 	}
-	// endregion
 
 	public boolean passed() {
 		return !errorDetected;
 	}
-
+	// endregion
 }
